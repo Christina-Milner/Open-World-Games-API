f348754ca20e0be5b9dd7523f1af3b5af141c5e7
Christina Milner <christina@christina-milner.dev>
2023-01-31 20:06:23 +0000

    Basic functionality there

--- server.js
+++ server.js
@@ -1,44 +1,182 @@
-const { request, response } = require('express')
+
 const express = require('express')
 const app = express()
-const cors = require('cors')
 const PORT = 8000
+const cors = require('cors')
 
 app.use(cors())
 
-const rappers = {
-    "21 savage": {
-        "age": 29,
-        "birthName": "Shéyaa Bin Abraham-Joseph",
-        "birthLocation": "London, England"
+const games = {
+    'skyrim': {
+        'title': "The Elder Scrolls V: Skyrim",
+        'release': 2011,
+        'pc': true,
+        'xbox': true,
+        'switch': true,
+        'playstation': true,
+        'metacritic': 94
     },
-    "chance the rapper": {
-        "age": 29,
-        "birthName": "Chancelor Bennett",
-        "birthLocation": "Chicago, Illinois"
+    'rdr2': {
+        'title': "Red Dead Redemption 2",
+        'release': 2018,
+        'pc': true,
+        'xbox': true,
+        'switch': false,
+        'playstation': true,
+        'metacritic': 93
     },
-    "unknown": {
-        "age": 0,
-        "birthName": "unknown",
-        "birthLocation": "unknown"
-    }
-}
+    'botw': {
+        'title': "The Legend of Zelda: Breath of the Wild",
+        'release': 2017,
+        'pc': false,
+        'xbox': false,
+        'switch': true,
+        'playstation': false,
+        'metacritic': 97
+    },
+    'gta5': {
+        'title': "Grand Theft Auto V",
+        'release': 2013,
+        'pc': true,
+        'xbox': true,
+        'switch': false,
+        'playstation': true,
+        'metacritic': 96
+    },
+    'witcher': {
+        'title': "The Witcher 3: Wild Hunt",
+        'release': 2015,
+        'pc': true,
+        'xbox': true,
+        'switch': true,
+        'playstation': true,
+        'metacritic': 93
+    },
+    'eldenRing': {
+        'title': "Elden Ring",
+        'release': 2022,
+        'pc': true,
+        'xbox': true,
+        'switch': false,
+        'playstation': true,
+        'metacritic': 94
+    },
+    'noMansSky': {
+        'title': "No Man's Sky",
+        'release': 2016,
+        'pc': true,
+        'xbox': true,
+        'switch': true,
+        'playstation': true,
+        'metacritic': 61
+    },
+    'mgs5': {
+        'title': "Metal Gear Solid V: The Phantom Pain",
+        'release': 2015,
+        'pc': true,
+        'xbox': true,
+        'switch': false,
+        'playstation': true,
+        'metacritic': 91
+    },
+    'hzd': {
+        'title': "Horizon Zero Dawn",
+        'release': 2017,
+        'pc': true,
+        'xbox': false,
+        'switch': false,
+        'playstation': true,
+        'metacritic': 89
+    },
+    'spiderman': {
+        'title': "Marvel's Spider-Man",
+        'release': 2018,
+        'pc': true,
+        'xbox': false,
+        'switch': false,
+        'playstation': true,
+        'metacritic': 87
+    },
+    'deathStranding': {
+        'title': "Death Stranding",
+        'release': 2019,
+        'pc': true,
+        'xbox': false,
+        'switch': false,
+        'playstation': true,
+        'metacritic': 82
+    },
+    'fallout4': {
+        'title': "Fallout 4",
+        'release': 2015,
+        'pc': true,
+        'xbox': true,
+        'switch': false,
+        'playstation': true,
+        'metacritic': 84
+    },
+    'daysGone': {
+        'title': "Days Gone",
+        'release': 2019,
+        'pc': true,
+        'xbox': false,
+        'switch': false,
+        'playstation': true,
+        'metacritic': 76
+    },
+    'cyberpunk': {
+        'title': "Cyberpunk 2077",
+        'release': 2020,
+        'pc': true,
+        'xbox': true,
+        'switch': false,
+        'playstation': true,
+        'metacritic': 86
+    },
+    'ghost': {
+        'title': "Ghost of Tsushima",
+        'release': 2020,
+        'pc': false,
+        'xbox': false,
+        'switch': false,
+        'playstation': true,
+        'metacritic': 83
+    },
 
+}
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/api/:name', (req, res) => {
-    const rapperName = req.params.name.toLowerCase()
-    if (rappers[rapperName]) {
-        res.json(rappers[rapperName])
-    } else {
-        res.json(rappers['unknown'])
+app.get('/api', (req, res) => {
+    res.json(games)
+})
+
+
+app.get('/api/rating/:score', (req, res) => {
+    const score = Number(req.params.score)
+    let result = {}
+    for (let game in games) {
+        if (games[game]['metacritic'] >= score) {
+            result[game] = games[game]
+        }
     }
+    res.json(result)
+})
+
+app.get('/api/platform/:platform', (req, res) => {
+    const platform = req.params.platform.toLowerCase()
+    let result = {}
+    for (let game in games) {
+        if (games[game][platform]) {
+            result[game] = games[game]
+        }
+    }
+    res.json(result)
 })
 
 
 app.listen(process.env.PORT || PORT, () => {
-    console.log(`The server is now running on port ${PORT}!`)
+    console.log(`The server is running on ${PORT}! You better go catch it!`)
 })
