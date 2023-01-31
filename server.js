e31bae1271355488de97afb71bbb51450bc1ef9a
Christina Milner <christina@christina-milner.dev>
2023-01-31 19:36:25 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const { request, response } = require('express')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 8000
+
+app.use(cors())
+
+const rappers = {
+    "21 savage": {
+        "age": 29,
+        "birthName": "Shéyaa Bin Abraham-Joseph",
+        "birthLocation": "London, England"
+    },
+    "chance the rapper": {
+        "age": 29,
+        "birthName": "Chancelor Bennett",
+        "birthLocation": "Chicago, Illinois"
+    },
+    "unknown": {
+        "age": 0,
+        "birthName": "unknown",
+        "birthLocation": "unknown"
+    }
+}
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (req, res) => {
+    const rapperName = req.params.name.toLowerCase()
+    if (rappers[rapperName]) {
+        res.json(rappers[rapperName])
+    } else {
+        res.json(rappers['unknown'])
+    }
+})
+
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`The server is now running on port ${PORT}!`)
+})
